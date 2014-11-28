bf9c84772fc2b16834947190f3710e2650a2894c
rajeshwarpatlolla <rajeshwar.patlolla@fissionlabs.in>
2014-11-28 14:53:22 +0530

    First commit.

---
+++ www/js/controllers.js
@@ -0,0 +1,93 @@
+angular.module('starter.controllers', [])
+
+    .controller('DashCtrl', function ($scope, $ionicPopup) {
+
+        $scope.slots = [
+            {epochTime: 12600, step: 15},
+            {epochTime: 54900, step: 1}
+        ];
+
+        $scope.showTimePickerModal = function (obj, str) {
+
+            $scope.time = { hours: 0, minutes: 0, meridian: "" };
+
+            var objDate = new Date(obj.epochTime * 1000);       // Epoch time in milliseconds.
+
+            $scope.time.meridian = (objDate.getUTCHours() > 11) ? "PM" : "AM";
+            $scope.time.hours = (objDate.getUTCHours() > 12) ? ((objDate.getUTCHours() - 12)) : (objDate.getUTCHours());
+            $scope.time.minutes = (objDate.getUTCMinutes());
+
+            $scope.increaseHours = function () {
+                if ($scope.time.hours != 12) {
+                    $scope.time.hours += 1;
+                } else {
+                    $scope.time.hours = 1;
+                }
+            };
+
+            $scope.decreaseHours = function () {
+                if ($scope.time.hours != 1) {
+                    $scope.time.hours -= 1;
+                } else {
+                    $scope.time.hours = 12;
+                }
+            };
+
+            $scope.increaseMinutes = function () {
+                if ($scope.time.minutes != (60 - obj.step)) {
+                    $scope.time.minutes += obj.step;
+                } else {
+                    $scope.time.minutes = 0;
+                }
+            };
+
+            $scope.decreaseMinutes = function () {
+                if ($scope.time.minutes != 0) {
+                    $scope.time.minutes -= obj.step;
+                } else {
+                    $scope.time.minutes = 60 - obj.step;
+                }
+            };
+
+            $scope.changeMeridian = function () {
+                $scope.time.meridian = ($scope.time.meridian === "AM") ? "PM" : "AM";
+            };
+
+            $ionicPopup.show({
+                templateUrl: 'my-time-picker.tpl.html',
+                title: '<strong>Select Time</strong>',
+                subTitle: '',
+                scope: $scope,
+                buttons: [
+                    { text: 'Cancel' },
+                    {
+                        text: 'Set',
+                        type: 'button-positive',
+                        onTap: function (e) {
+
+                            $scope.loadingContent = true;
+
+                            var totalSec = ($scope.time.hours * 60 * 60) + ($scope.time.minutes * 60);
+                            console.log("totalSec 1",totalSec);
+
+                            if ($scope.time.meridian === "AM") {
+                                totalSec += 0;
+                            } else if ($scope.time.meridian === "PM") {
+                                totalSec += 43200;
+                            }
+                            console.log("totalSec 2",totalSec);
+                            obj.epochTime = totalSec;
+
+                        }
+                    }
+                ]
+            })
+        };
+
+    })
+
+    .controller('FriendsCtrl', function ($scope) {
+    })
+
+    .controller('AccountCtrl', function ($scope) {
+    });
