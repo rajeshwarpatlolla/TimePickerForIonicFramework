79a2bd19e2ff4a179c5a96b67a132d795695fa1a
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2014-12-04 14:59:35 +0530

    v0.2.0 time will be disapled.

--- www/js/app.js
+++ www/js/app.js
@@ -6,69 +6,114 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-    // setup an abstract state for the tabs directive
-    .state('tab', {
-      url: "/tab",
-      abstract: true,
-      templateUrl: "templates/tabs.html"
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
     })
 
-    // Each tab has its own nav history stack:
+    .config(function ($stateProvider, $urlRouterProvider) {
 
-    .state('tab.timepicker', {
-      url: '/timepicker',
-      views: {
-        'tab-timepicker': {
-          templateUrl: 'templates/tab-timepicker.html',
-          controller: 'DashCtrl'
-        }
-      }
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        $stateProvider
+
+            // setup an abstract state for the tabs directive
+            .state('tab', {
+                url: "/tab",
+                abstract: true,
+                templateUrl: "templates/tabs.html"
+            })
+
+            // Each tab has its own nav history stack:
+
+            .state('tab.timepicker', {
+                url: '/timepicker',
+                views: {
+                    'tab-timepicker': {
+                        templateUrl: 'templates/tab-timepicker.html',
+                        controller: 'DashCtrl'
+                    }
+                }
+            })
+
+            .state('tab.reference', {
+                url: '/reference',
+                views: {
+                    'tab-reference': {
+                        templateUrl: 'templates/tab-reference.html',
+                        controller: 'FriendsCtrl'
+                    }
+                }
+            })
+
+            .state('tab.contact', {
+                url: '/contact',
+                views: {
+                    'tab-contact': {
+                        templateUrl: 'templates/tab-contact.html',
+                        controller: 'AccountCtrl'
+                    }
+                }
+            });
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/tab/timepicker');
+
     })
 
-    .state('tab.reference', {
-      url: '/reference',
-      views: {
-        'tab-reference': {
-          templateUrl: 'templates/tab-reference.html',
-          controller: 'FriendsCtrl'
-        }
-      }
-    })
+    .directive('standardTime', function () {
+        return {
+            restrict: 'AE',
+            replace: true,
+            scope: {
+                etime: '=etime'
+            },
+            template: "<strong>{{stime}}</strong>",
+            link: function (scope, elem, attrs) {
 
-    .state('tab.contact', {
-      url: '/contact',
-      views: {
-        'tab-contact': {
-          templateUrl: 'templates/tab-contact.html',
-          controller: 'AccountCtrl'
-        }
-      }
+                scope.stime = epochParser(scope.etime, 'time');
+
+                function prependZero(param) {
+                    if (String(param).length < 2) {
+                        return "0" + String(param);
+                    }
+                    return param;
+                }
+
+                function epochParser(val, opType) {
+                    if (val === null) {
+                        return "00:00";
+                    } else {
+                        var meridian = ['AM', 'PM'];
+
+                        if (opType === 'time') {
+                            var hours = parseInt(val / 3600);
+                            var minutes = (val / 60) % 60;
+                            var hoursRes = hours > 12 ? (hours - 12) : hours;
+
+                            var currentMeridian = meridian[parseInt(hours / 12)];
+
+                            return (prependZero(hoursRes) + ":" + prependZero(minutes) + " " + currentMeridian);
+                        }
+                    }
+                }
+
+                scope.$watch('etime', function (newValue, oldValue) {
+                    scope.stime = epochParser(scope.etime, 'time');
+                });
+
+            }
+        };
     });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/timepicker');
-
-});
 
