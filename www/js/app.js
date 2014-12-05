ed814fcef6cf95adc0004f4d536b39ef95178aaf
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2014-12-05 22:58:06 +0530

    24 hour format has been included.

--- www/js/app.js
+++ www/js/app.js
@@ -72,7 +72,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
     })
 
-    .directive('standardTime', function () {
+    .directive('standardTimeMeridian', function () {
         return {
             restrict: 'AE',
             replace: true,
@@ -105,6 +105,46 @@ angular.module('starter', ['ionic', 'starter.controllers'])
                             var currentMeridian = meridian[parseInt(hours / 12)];
 
                             return (prependZero(hoursRes) + ":" + prependZero(minutes) + " " + currentMeridian);
+                        }
+                    }
+                }
+
+                scope.$watch('etime', function (newValue, oldValue) {
+                    scope.stime = epochParser(scope.etime, 'time');
+                });
+
+            }
+        };
+    })
+
+    .directive('standardTimeNoMeridian', function () {
+        return {
+            restrict: 'AE',
+            replace: true,
+            scope: {
+                etime: '=etime'
+            },
+            template: "<strong>{{stime}}</strong>",
+            link: function (scope, elem, attrs) {
+
+                scope.stime = epochParser(scope.etime, 'time');
+
+                function prependZero(param) {
+                    if (String(param).length < 2) {
+                        return "0" + String(param);
+                    }
+                    return param;
+                }
+
+                function epochParser(val, opType) {
+                    if (val === null) {
+                        return "00:00";
+                    } else {
+                        if (opType === 'time') {
+                            var hours = parseInt(val / 3600);
+                            var minutes = (val / 60) % 60;
+
+                            return (prependZero(hours) + ":" + prependZero(minutes));
                         }
                     }
                 }
