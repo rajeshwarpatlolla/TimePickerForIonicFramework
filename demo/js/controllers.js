40e824f821071293c6c854516824c1bcd04a8100
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2015-06-13 16:33:19 +0530

    ionic-timepicker v0.2.0 added

--- demo/js/controllers.js
+++ demo/js/controllers.js
@@ -1,16 +1,24 @@
 angular.module('starter.controllers', [])
 
-    .controller('DashCtrl', function ($scope, $ionicPopup) {
+  .controller('DashCtrl', function ($scope, $ionicPopup) {
 
-        $scope.slots = [
-            {epochTime: 12600, step: 15, format: 12},
-            {epochTime: 54900, step: 1, format: 24}
-        ];
+    $scope.slots = [
+      {epochTime: 12600, step: 15, format: 12},
+      {epochTime: 54900, step: 1, format: 24}
+    ];
 
-    })
+    $scope.timePickerCallback = function (val) {
+      if (typeof (val) === 'undefined') {
+        console.log('Time not selected');
+      } else {
+        console.log('Selected time is : ', val);
+      }
+    };
 
-    .controller('FriendsCtrl', function ($scope) {
-    })
+  })
 
-    .controller('AccountCtrl', function ($scope) {
-    });
+  .controller('FriendsCtrl', function ($scope) {
+  })
+
+  .controller('AccountCtrl', function ($scope) {
+  });
