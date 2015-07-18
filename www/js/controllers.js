2a39e543f2aa7374331f437929cef2ac108689db
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2015-07-18 12:52:56 +0530

    changes in templates

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,7 +3,7 @@ angular.module('starter.controllers', [])
   .controller('DashCtrl', function ($scope, $ionicPopup) {
 
     $scope.slots = [
-      {epochTime: 12600, step: 15, format: 12},
+      {epochTime: 10800, step: 15, format: 12},
       {epochTime: 54900, step: 1, format: 24}
     ];
 
