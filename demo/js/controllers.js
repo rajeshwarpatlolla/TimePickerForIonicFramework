3040fc0c84e6f67126d63b37d0d4f8edac8b0e80
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2015-08-21 20:03:23 +0530

    ionic-timepicker v0.3.0 installed

--- demo/js/controllers.js
+++ demo/js/controllers.js
@@ -2,18 +2,53 @@ angular.module('starter.controllers', [])
 
   .controller('DashCtrl', function ($scope, $ionicPopup) {
 
-    $scope.slots = [
-      {epochTime: 12600, step: 15, format: 12},
-      {epochTime: 54900, step: 1, format: 24}
-    ];
+    $scope.timePickerObject12Hour = {
+      inputEpochTime: ((new Date()).getHours() * 60 * 60),  //Optional
+      step: 15,  //Optional
+      format: 12,  //Optional
+      titleLabel: '12-hour Format',  //Optional
+      closeLabel: 'Close',  //Optional
+      setLabel: 'Set',  //Optional
+      setButtonType: 'button-positive',  //Optional
+      closeButtonType: 'button-stable',  //Optional
+      callback: function (val) {    //Mandatory
+        timePicker12Callback(val);
+      }
+    };
 
-    $scope.timePickerCallback = function (val) {
+    $scope.timePickerObject24Hour = {
+      inputEpochTime: ((new Date()).getHours() * 60 * 60),  //Optional
+      step: 10,  //Optional
+      format: 24,  //Optional
+      titleLabel: '24-hour Format',  //Optional
+      closeLabel: 'Cancel',  //Optional
+      setLabel: 'Select',  //Optional
+      setButtonType: 'button-balanced',  //Optional
+      closeButtonType: 'button-positive',  //Optional
+      callback: function (val) {    //Mandatory
+        timePicker24Callback(val);
+      }
+    };
+
+    function timePicker12Callback(val) {
       if (typeof (val) === 'undefined') {
         console.log('Time not selected');
       } else {
-        console.log('Selected time is : ', val);
+        $scope.timePickerObject12Hour.inputEpochTime = val;
+        var selectedTime = new Date(val * 1000);
+        console.log('Selected epoch is : ', val, 'and the time is ', selectedTime.getUTCHours(), ':', selectedTime.getUTCMinutes(), 'in UTC');
       }
-    };
+    }
+
+    function timePicker24Callback(val) {
+      if (typeof (val) === 'undefined') {
+        console.log('Time not selected');
+      } else {
+        $scope.timePickerObject24Hour.inputEpochTime = val;
+        var selectedTime = new Date(val * 1000);
+        console.log('Selected epoch is : ', val, 'and the time is ', selectedTime.getUTCHours(), ':', selectedTime.getUTCMinutes(), 'in UTC');
+      }
+    }
 
   })
 
