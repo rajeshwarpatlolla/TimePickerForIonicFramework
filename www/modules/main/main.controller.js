e0060d95a0c199ceaf4e7d7a776c6dc4466dd082
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2016-03-27 20:35:25 +0530

    changes in the callback

--- www/modules/main/main.controller.js
+++ www/modules/main/main.controller.js
@@ -5,7 +5,12 @@ angular.module('starter.controllers', [])
     $scope.openTimePicker1 = function () {
       var ipObj1 = {
         callback: function (val) {
-          console.log('Return value from the datepicker popup is : ' + val, new Date(val));
+          if (typeof (val) === 'undefined') {
+            console.log('Time not selected');
+          } else {
+            var selectedTime = new Date(val * 1000);
+            console.log('Selected epoch is : ', val, 'and the time is ', selectedTime.getUTCHours(), 'H :', selectedTime.getUTCMinutes(), 'M');
+          }
         },
         inputTime: 50400,
         format: 12,
@@ -17,7 +22,12 @@ angular.module('starter.controllers', [])
     $scope.openTimePicker2 = function () {
       var ipObj1 = {
         callback: function (val) {
-          console.log('Return value from the datepicker popup is : ' + val, new Date(val));
+          if (typeof (val) === 'undefined') {
+            console.log('Time not selected');
+          } else {
+            var selectedTime = new Date(val * 1000);
+            console.log('Selected epoch is : ', val, 'and the time is ', selectedTime.getUTCHours(), 'H :', selectedTime.getUTCMinutes(), 'M');
+          }
         },
         inputTime: ((new Date()).getHours() * 60 * 60 + (new Date()).getMinutes() * 60),
         format: 24
